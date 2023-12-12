6ab0af10a7178d56dd54e5ed4053a10ec4f2aad5
JBasias <jbasias@gmail.com>
2023-12-12 18:28:46 -0500

    a needed commit

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,13 +1,34 @@
 
-function bubbleSort(arr) {
+function bubbleSort(A) {
 
     // Iterate through the array
+
+    let Is=true; let cry;
+
+    while(Is)
+    {
+       Is=false;
+       for(let i=0;i<A.length-1;i++)
+       {
+           if(A[i]>A[i+1])
+            {
+                Is=true;
+                cry=A[i];
+                A[i] = A[i+1];
+                A[i+1] =cry;
+                console.log(A.join(","));
+                //break;
+            }
+       }
+
+       //console.log(A.join(","));
+    }
 
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
+       // console.log(A.join(","));
 
     // If you get to the end of the array and no swaps have occurred, return
 
